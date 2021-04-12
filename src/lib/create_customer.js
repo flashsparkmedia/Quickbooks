3dbe0842e60079830088c6270442dfc9e58044b5
Brad Rumack <bradrumack@Brads-Mac-mini.local>
2021-04-11 21:02:59 -0700

    promisify methods

--- src/lib/create_customer.js
+++ src/lib/create_customer.js
@@ -1,31 +1,63 @@
-async function createCustomer(options) {
-    if (!options) {
-        throw new Error('createCustomer options not provided.')
-    }
+// async function createCustomer(options) {
+//     if (!options) {
+//         throw new Error('createCustomer options not provided.')
+//     }
 
-    try {
-        const response = await this.makeRequest({
-            url: `${this.BASE_URL_WEB}/v3/company/${this.realm_id}/customer?minorversion=57`,
-            method: 'POST',
-            data: {
-                DisplayName: options.displayName,
-                FamilyName: options.familyName,
-                GivenName: options.givenName,
-                PrimaryEmailAddr: {
-                    Address: options.email
-                }
-            }
-        })
-        return response.Customer
-    } catch(e) {
-        if (e && e.Fault) {
-            const errorsArray = e.Fault.Error
-            const errors = errorsArray.map(error => error.Message).join(' ')
-            throw new Error(errors)
-        } else {
-            throw new Error(e)
+//     try {
+//         const response = await this.makeRequest({
+//             url: `${this.BASE_URL_WEB}/v3/company/${this.realm_id}/customer?minorversion=57`,
+//             method: 'POST',
+//             data: {
+//                 DisplayName: options.displayName,
+//                 FamilyName: options.familyName,
+//                 GivenName: options.givenName,
+//                 PrimaryEmailAddr: {
+//                     Address: options.email
+//                 }
+//             }
+//         })
+//         return response.Customer
+//     } catch(e) {
+//         if (e.Fault) {
+//             const errorsArray = e.Fault.Error
+//             const errors = errorsArray.map(error => error.Message).join(' ')
+//             throw new Error(errors)
+//         } else {
+//             throw new Error(e)
+//         }
+//     }
+// }
+
+async function createCustomer(options) {
+    return new Promise((resolve, reject) => {
+        if (!options) {
+            reject('createCustomer options not provided.')
         }
-    }
+
+        try {
+            const response = await this.makeRequest({
+                url: `${this.BASE_URL_WEB}/v3/company/${this.realm_id}/customer?minorversion=57`,
+                method: 'POST',
+                data: {
+                    DisplayName: options.displayName,
+                    FamilyName: options.familyName,
+                    GivenName: options.givenName,
+                    PrimaryEmailAddr: {
+                        Address: options.email
+                    }
+                }
+            })
+            resolve(response.Customer)
+        } catch(e) {
+            if (e.Fault) {
+                const errorsArray = e.Fault.Error
+                const errors = errorsArray.map(error => error.Message).join(' ')
+                reject(errors)
+            } else {
+                reject(e)
+            }
+        }
+    })   
 }
 
 module.exports = createCustomer
--- src/lib/create_vendor.js
+++ src/lib/create_vendor.js
@@ -1,29 +1,59 @@
+// async function createVendor(options) {
+//     if (!options) {
+//         throw new Error('createVendor options not provided.')
+//     }
+
+//     try {
+//         const response = await this.makeRequest({
+//             url: `${this.BASE_URL_WEB}/v3/company/${this.realm_id}/vendor?minorversion=57`,
+//             method: 'POST',
+//             data: {
+//                 DisplayName: options.displayName,
+//                 FamilyName: options.familyName,
+//                 GivenName: options.givenName,
+//                 Vendor1099: true,
+//                 PrimaryEmailAddr: {
+//                     Address: options.email
+//                 }
+//             }
+//         })
+
+//         return response.Vendor
+//     } catch(e) {
+//         const errorsArray = e.Fault.Error
+//         const errors = errorsArray.map(error => error.Message).join(' ')
+//         throw new Error(errors)
+//     }
+// }
+
 async function createVendor(options) {
-    if (!options) {
-        throw new Error('createVendor options not provided.')
-    }
+    return new Promise((resolve, reject) => {
+        if (!options) {
+           reject('createVendor options not provided.')
+        }
 
-    try {
-        const response = await this.makeRequest({
-            url: `${this.BASE_URL_WEB}/v3/company/${this.realm_id}/vendor?minorversion=57`,
-            method: 'POST',
-            data: {
-                DisplayName: options.displayName,
-                FamilyName: options.familyName,
-                GivenName: options.givenName,
-                Vendor1099: true,
-                PrimaryEmailAddr: {
-                    Address: options.email
+        try {
+            const response = await this.makeRequest({
+                url: `${this.BASE_URL_WEB}/v3/company/${this.realm_id}/vendor?minorversion=57`,
+                method: 'POST',
+                data: {
+                    DisplayName: options.displayName,
+                    FamilyName: options.familyName,
+                    GivenName: options.givenName,
+                    Vendor1099: true,
+                    PrimaryEmailAddr: {
+                        Address: options.email
+                    }
                 }
-            }
-        })
-
-        return response.Vendor
-    } catch(e) {
-        const errorsArray = e.Fault.Error
-        const errors = errorsArray.map(error => error.Message).join(' ')
-        throw new Error(errors)
-    }
+            })
+    
+            resolve(esponse.Vendor)
+        } catch(e) {
+            const errorsArray = e.Fault.Error
+            const errors = errorsArray.map(error => error.Message).join(' ')
+            reject(errors)
+        }
+    })
 }
 
 module.exports = createVendor
